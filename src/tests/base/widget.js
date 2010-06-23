86c5c6571e4dc95fb0fab72da48111d3e656510c
wolfram.kriesing <wolfram.kriesing@1690c1e1-514a-e7be-8591-f3815e2cc7b6>
2010-06-23 07:40:10 +0000

    Fixed showNotification() tests, must have two params per spec!

--- src/tests/base/widget.js
+++ src/tests/base/widget.js
@@ -3,22 +3,26 @@
 	dohx.add({name:"window",
 		mqcExecutionOrderBaseOffset:340000, // This number is the base offset for the execution order, the test ID gets added. Never change this number unless you know what you are doing.
 		tests:[
+			//
+			//	showNotification
+			//	spec: http://www.w3.org/TR/2009/WD-widgets-apis-20090423/#the-shownotification-method
+			//
 			{
 				id: 100,
 				name:"showNotification - simple",
 				requiredObjects:["widget.showNotification"],
-				expectedResult:"Did you see a popup telling you something nice?",
+				expectedResult:"Did you see a popup with 1) headline 'Hello headline' and 2) the message telling you something nice?",
 				test:function(t){
-					widget.showNotification("Hello, have a nice day!!!");
+					widget.showNotification("Hello headline", "Hello, have a nice day!!!");
 				}
 			},
 			{
 				id: 200,
 				name:"showNotification - two lines",
 				requiredObjects:["widget.showNotification"],
-				expectedResult:"Did you see a popup, with a TWO LINE message?",
+				expectedResult:"Did you see a popup, with 1) the headline and 2) a TWO LINE message?",
 				test:function(t){
-					widget.showNotification("Hello, have a nice day!!!\nA really nice day!");
+					widget.showNotification("Hello headline", "Hello, have a nice day!!!\nA really nice day!");
 				}
 			},
 			{
@@ -27,7 +31,7 @@
 				requiredObjects:["widget.showNotification"],
 				timeout:30 * 1000, // Give the user 30secs
 				test:function(t){
-					widget.showNotification("Just click me away, thanks.", function(){
+					widget.showNotification("Hello headline", "Just click me away, thanks.", function(){
 						t.success("Callback fired.");
 					});
 				}
