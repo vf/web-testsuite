cf430b657f0de72464677e8780aedb3767865425
wolfram.kriesing <wolfram.kriesing@1690c1e1-514a-e7be-8591-f3815e2cc7b6>
2010-09-08 14:12:47 +0000

    Providing more helper functioanlity.

--- src/util/setupHelper.js
+++ src/util/setupHelper.js
@@ -13,7 +13,18 @@
 					"This widget will show some relevant information which you might need to configure your test widget(s) properly.",
 				],
 				test:function(t){
-					t.success();
+					t.success(true);
+				}
+			},
+			{
+				id:2,
+				name:"config info",
+				test:function(t){
+					if (config._meta.isCustomConfiguration){
+						t.success(config._meta);
+					}else{
+						t.failure("Unconfigured, userAgent: " + window.navigator.userAgent);
+					}
 				}
 			},
 			
@@ -39,6 +50,30 @@
 						}
 					}
 					pim.findAddressBookItems(addr, 0, 1);
+				},
+				tearDown:function(){
+					delete pim.onAddressBookItemsFound;
+				}
+			},
+			{
+				id:200,
+				name:"config.validCalendarItemId",
+				test:function(t){
+					var item = new pim.CalendarItem();
+					item.eventName = "*";
+					Widget.PIM.onCalendarItemsFound = function(items){
+						if (items.length==0){
+							t.failure("No items found :(");
+						} else {
+							var ids = [];
+							for (var i=0; i<items.length; i++){
+								ids.push(items[i].calendarItemId);
+								if (i>10) break; // 10 at most
+							}
+							t.success("Some valid IDs: "+ ids.join(", "));
+						}
+					}
+					pim.findCalendarItems(item, 0, 1);
 				},
 				tearDown:function(){
 					delete pim.onAddressBookItemsFound;
