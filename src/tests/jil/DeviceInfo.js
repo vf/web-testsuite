5667b36c58c585945f44e7d9921bf89d5409c9e1
wolfram.kriesing <wolfram.kriesing@1690c1e1-514a-e7be-8591-f3815e2cc7b6>
2010-09-09 12:51:07 +0000

    verify values

--- src/tests/jil/DeviceInfo.js
+++ src/tests/jil/DeviceInfo.js
@@ -17,7 +17,8 @@
 								 "'ownerInfo' is not an instance of 'Widget.PIM.AddressBookItem'.");
 					return wdd.ownerInfo;
 				}
-			},{
+			},
+			{
 				id:200,
 				name:"phoneColorDepthDefault",
 				requiredObjects:["Widget.Device.DeviceInfo.phoneColorDepthDefault"],
@@ -26,7 +27,17 @@
 					t.assertTrue(util.isNumber(value));
 					return value;
 				}
-			},{
+			},
+			{
+				id:210,
+				name:"phoneColorDepthDefault - verify",
+				requiredObjects:["Widget.Device.DeviceInfo.phoneColorDepthDefault"],
+				expectedResult:"Is the above the proper color depth? (May need to look it up in the device's specification)",
+				test:function(t){
+					dohx.showInfo("API reports:", wdd.phoneColorDepthDefault);
+				}
+			},
+			{
 				id:300,
 				name:"phoneFirmware",
 				requiredObjects:["Widget.Device.DeviceInfo.phoneFirmware"],
@@ -35,7 +46,17 @@
 					t.assertNotEqual("", ret);
 					return ret; // Return it, so the user sees the positive result.
 				}
-			},{
+			},
+			{
+				id:310,
+				name:"phoneFirmware - verify",
+				requiredObjects:["Widget.Device.DeviceInfo.phoneFirmware"],
+				expectedResult:"Is the reported value correct?",
+				test:function(t){
+					dohx.showInfo("API reports:", wdd.phoneFirmware);
+				}
+			},
+			{
 				id:400,
 				name:"phoneManufacturer",
 				requiredObjects:["Widget.Device.DeviceInfo.phoneManufacturer"],
@@ -44,7 +65,17 @@
 					t.assertNotEqual("", ret);
 					return ret; // Return it, so the user sees the positive result.
 				}
-			},{
+			},
+			{
+				id:410,
+				name:"phoneManufacturer - verify",
+				requiredObjects:["Widget.Device.DeviceInfo.phoneManufacturer"],
+				expectedResult:"Is the reported value correct?",
+				test:function(t){
+					dohx.showInfo("API reports:", wdd.phoneManufacturer);
+				}
+			},
+			{
 				id:500,
 				name:"phoneModel",
 				requiredObjects:["Widget.Device.DeviceInfo.phoneModel"],
@@ -53,7 +84,17 @@
 					t.assertNotEqual("", ret);
 					return ret; // Return it, so the user sees the positive result.
 				}
-			},{
+			},
+			{
+				id:510,
+				name:"phoneModel - verify",
+				requiredObjects:["Widget.Device.DeviceInfo.phoneModel"],
+				expectedResult:"Is the reported value correct?",
+				test:function(t){
+					dohx.showInfo("API reports:", wdd.phoneModel);
+				}
+			},
+			{
 				id:600,
 				name:"phoneOS",
 				requiredObjects:["Widget.Device.DeviceInfo.phoneOS"],
@@ -62,7 +103,17 @@
 					t.assertNotEqual("", ret);
 					return ret; // Return it, so the user sees the positive result.
 				}
-			},{
+			},
+			{
+				id:610,
+				name:"phoneOS - verify",
+				requiredObjects:["Widget.Device.DeviceInfo.phoneOS"],
+				expectedResult:"Is the reported value correct?",
+				test:function(t){
+					dohx.showInfo("API reports:", wdd.phoneOS);
+				}
+			},
+			{
 				id:700,
 				name:"phoneScreenHeightDefault",
 				requiredObjects:["Widget.Device.DeviceInfo.phoneScreenHeightDefault"],
@@ -71,7 +122,16 @@
 					t.assertTrue(util.isNumber(value));
 					return value;
 				}
-			},{
+			},
+			{
+				id:710,
+				name:"phoneScreenHeightDefault - verify",
+				requiredObjects:["Widget.Device.DeviceInfo.phoneFirmware"],
+				test:function(t){
+					t.assertEqual(wdd.phoneScreenHeightDefault, window.screen.availHeight);
+				}
+			},
+			{
 				id:800,
 				name:"phoneScreenWidthDefault",
 				requiredObjects:["Widget.Device.DeviceInfo.phoneScreenWidthDefault"],
@@ -80,7 +140,16 @@
 					t.assertTrue(util.isNumber(value));
 					return value;
 				}
-			},{
+			},
+			{
+				id:810,
+				name:"phoneScreenHeightDefault",
+				requiredObjects:["Widget.Device.DeviceInfo.phoneScreenHeightDefault"],
+				test:function(t){
+					t.assertEqual(wdd.phoneScreenWidthDefault, window.screen.availWidth);
+				}
+			},
+			{
 				id:900,
 				name:"phoneSoftware",
 				requiredObjects:["Widget.Device.DeviceInfo.phoneSoftware"],
@@ -89,7 +158,17 @@
 					t.assertNotEqual("", value);
 					return value;
 				}
-			},{
+			},
+			{
+				id:910,
+				name:"phoneSoftware - verify",
+				requiredObjects:["Widget.Device.DeviceInfo.phoneSoftware"],
+				expectedResult:"Is the reported value correct?",
+				test:function(t){
+					dohx.showInfo("API reports:", wdd.phoneSoftware);
+				}
+			},
+			{
 				id:1000,
 				name:"totalMemory",
 				requiredObjects:["Widget.Device.DeviceInfo.totalMemory"],
