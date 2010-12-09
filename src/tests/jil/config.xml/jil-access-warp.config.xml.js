d49144d1c8395ca86be8936fd7b903d2d4150593
wolfram.kriesing <wolfram.kriesing@1690c1e1-514a-e7be-8591-f3815e2cc7b6>
2010-12-09 17:36:58 +0000

    Added jil:access+warp tags tests

---
+++ src/tests/jil/config.xml/jil-access-warp.config.xml.js
@@ -0,0 +1,22 @@
+
+var features = [];
+var xml = xmlHelper.getXmlObject(config, features);
+//xml.@version = "0";
+//xml.widget.id =
+
+//
+//	Do all modifications to elements inside the default namespace (not prefixed with e.g. "jil:")
+//
+default xml namespace = "http://www.w3.org/ns/widgets";
+
+delete xml.access.@*;
+xml.access.@origin = "http://philstar.de/"
+xml.access.@subdomains = "true"
+
+//
+//	Do all modifications in the JIL namespace.
+//
+default xml namespace = "http://www.jil.org/ns/widgets1.2";
+// Explicitly make the tag to ONLY contain that one attribute "network".
+delete xml.access.@*;
+xml.access.@network = "true";
---
+++ src/tests/jil/config.xml/jil-access-warp.js
@@ -0,0 +1,58 @@
+(function(){
+	
+	var externalImageUrl = "http://philstar.de/vf/test.png";
+	
+	dohx.add({name:"Testing WARP &lt;access /&gt;",
+		mqcExecutionOrderBaseOffset:440000, // This number is the base offset for the execution order, the test ID gets added. Never change this number unless you know what you are doing.
+		tests:[
+			{
+				id:100,
+				name:"Use &lt;img src&gt; to load an external resource.",
+				expectedResult:"Did you see an image loading?",
+				test:function(t){
+					dohx.showInfo('Loading...<br /><img src="' + externalImageUrl + '" alt="external image" width="100">');
+				}
+			},
+			{
+				id:200,
+				name:"Use 'new Image()' to load an external resource.",
+				expectedResult:"Did you see an image loading?",
+				test:function(t){
+					dohx.showInfo('Loading ...<div id="_imgSrc_"></div>');
+					var img = new Image();
+					img.src = externalImageUrl;
+					img.width = "100";
+					setTimeout(function(){ // Let the showInfo() generate the DIV.
+						var el = util.byId("_imgSrc_");
+						el.appendChild(img);
+					}, 100);
+				}
+			},
+			{
+				id:300,
+				name:"Use XHR to load an external resource.",
+				test:function(t){
+					try{
+						var xhr = new XMLHttpRequest();
+						xhr.open("GET", "http://philstar.de/vf/test.png?nocache="+ (+new Date()), true);
+						xhr.onreadystatechange = function (){
+							if(xhr.readyState == 4){
+								if (xhr.status == 200){
+									t.success("finished with status " + xhr.status);
+								} else {
+									t.failure("xhr.status=" + xhr.status);
+								}
+							}
+						};
+						xhr.onerror = function(e){
+							t.failure(e.message || e);
+						};
+						xhr.send(null);
+					}catch(e){
+						t.failure(e.message || e);
+					}
+				}
+			}
+		]
+	});
+})();
