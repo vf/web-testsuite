4246855333c7dcfdbf4ba776fff613f82332d7fd
wolfram.kriesing <wolfram.kriesing@1690c1e1-514a-e7be-8591-f3815e2cc7b6>
2010-12-10 11:37:13 +0000

    Fixed tests, committed

--- src/tests/jil/config.xml/warp-jil-access.config.xml.js
+++ src/tests/jil/config.xml/warp-jil-access.config.xml.js
@@ -5,21 +5,18 @@ var xml = xmlHelper.getXmlObject(config, features);
 //xml.widget.id =
 
 //
+//	Do all modifications to elements inside the default namespace (not prefixed with e.g. "jil:")
+//
+default xml namespace = "http://www.w3.org/ns/widgets";
+
+delete xml.access.@*;
+xml.access.@origin = "http://philstar.de/"
+xml.access.@subdomains = "true"
+
+//
 //	Do all modifications in the JIL namespace.
 //
 default xml namespace = "http://www.jil.org/ns/widgets1.2";
 // Explicitly make the tag to ONLY contain that one attribute "network".
 delete xml.access.@*;
 xml.access.@network = "true";
-
-//
-//	Do all modifications to elements inside the default namespace (not prefixed with e.g. "jil:")
-//
-default xml namespace = "http://www.w3.org/ns/widgets";
-
-delete xml.access; // Remove it so it gets added to the end later, since the order is important for this test.
-
-delete xml.access.@*;
-xml.access.@origin = "http://philstar.de/"
-xml.access.@subdomains = "true"
-
