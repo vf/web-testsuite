e17be2addeecdb4320caa91fc10ef18ea4c14855
wolfram.kriesing <wolfram.kriesing@1690c1e1-514a-e7be-8591-f3815e2cc7b6>
2011-01-05 16:55:00 +0000

    Put the util files in the bundle too.

--- buildTestSuiteBundle.sh
+++ buildTestSuiteBundle.sh
@@ -21,6 +21,7 @@ mkdir $TMP_DIR
 
 #cp dist/jil-test-signed/test* $TMP_DIR
 cp dist/unsigned/test* $TMP_DIR
+cp dist/unsigned/util* $TMP_DIR
 cp ReleaseNotes.txt $TMP_DIR
 cp all-tests.xls $TMP_DIR
 cp -R src/test-audio $TMP_DIR/
