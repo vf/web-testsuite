0679fe318e8fd878c4100b3da953d4a8d9149be3
wolfram.kriesing <wolfram.kriesing@1690c1e1-514a-e7be-8591-f3815e2cc7b6>
2011-01-05 16:56:37 +0000

    Better test instructions.

--- src/tests/w3c/css/basics.js
+++ src/tests/w3c/css/basics.js
@@ -7,7 +7,7 @@
 			{
 				id:100,
 				name:"Selectors: nth-child",
-				expectedResult:"Does the inner element have a white background color, and the outer element a red background color?",
+				expectedResult:"Does the INNER element have a WHITE background color,<br />and the OUTER element a RED background color?",
 				test:function(t){
 					dohx.showInfo(	'<div id="test10">'+
 										'<div id="test11">Outer element'+
@@ -18,7 +18,7 @@
 			{
 				id:200,
 				name:"Selectors: + selector",
-				expectedResult:"Does the element have a red background color?",
+				expectedResult:"Does the element have a RED BACKGROUND color?",
 				test:function(t){
 					dohx.showInfo('<div id="test20"></div><div id="test21">The element</div>');
 				}
@@ -26,7 +26,7 @@
 			{
 				id:300,
 				name:"Selectors: + selector (2)",
-				expectedResult:"Does 'The element' have a black background color?",
+				expectedResult:"Does 'The element' have a BLACK BACKGROUND color?",
 				test:function(t){
 					dohx.showInfo('<div id="test20"></div><div id="test21">The element</div>');
 					var insertPoint = document.getElementById('test20');
@@ -38,7 +38,7 @@
 			{
 				id:400,
 				name:"Selectors: ':last-child' selector",
-				expectedResult:"Does the third item have a red background color?",
+				expectedResult:"Does the THIRD ITEM have a RED BACKGROUND color?",
 				test:function(t){
 					dohx.showInfo(
 								'<ul id="test400">'+
@@ -50,7 +50,7 @@
 			{
 				id:500,
 				name:"Selectors: ':last-child' selector (2)",
-				expectedResult:"Does the third item have a BLACK background color?",
+				expectedResult:"Does the THIRD ITEM have a BLACK BACKGROUND color?",
 				test:function(t){
 					dohx.showInfo(
 								'<ul id="test500">'+
@@ -67,7 +67,7 @@
 			{
 				id:600,
 				name:"Selectors: :disabled",
-				expectedResult:"Is the text input red and is it NOT possible to write text into it?",
+				expectedResult:"Is the text INPUT RED<br />and is it NOT EDITABLE?",
 				test:function(t){
 					dohx.showInfo('<input type="text" id="test40" disabled style="font-size:2em;">');
 				}
@@ -77,13 +77,13 @@
 				name:"Selectors: :checked",
 				expectedResult:"Check the checkbox. Does it move to the right?",
 				test:function(t){
-					dohx.showInfo('<input type="checkbox" id="test41">');
+					dohx.showInfo('<input type="checkbox" id="test700">');
 				}
 			},
 			{
 				id:800,
 				name:"Selectors: attributes",
-				expectedResult:"Does the first element have a red background color, and the second a black?",
+				expectedResult:"Does the FIRST element have a RED BACKGROUND color,<br />and the SECOND a BLACK?",
 				test:function(t){
 					dohx.showInfo(
 								'<div class="test50" align="right">The first element</div>'+
@@ -94,7 +94,7 @@
 			{
 				id:900,
 				name:"Selectors: attributes",
-				expectedResult:"Does this element have a red background and underlined text?",
+				expectedResult:"Does this element have a RED BACKGROUND<br/>and UNDERLINED text?",
 				test:function(t){
 					dohx.showInfo('<div class="test51 first">The element</div>');
 				}
@@ -102,7 +102,7 @@
 			{
 				id:1000,
 				name:"Selectors: :nth-child",
-				expectedResult:"Does this element have a red background and underlined text?",
+				expectedResult:"Does this element have a RED BACKGROUND<br />and UNDERLINED text?",
 				test:function(t){
 					dohx.showInfo('<div class="test51 first">The element</div>');
 				}
@@ -110,7 +110,7 @@
 			{
 				id:1100,
 				name:"Selectors: :nth-child(2n)",
-				expectedResult:"Do the second and fourth items have a red background?",
+				expectedResult:"Do the SECOND AND FOURTH items have a RED BACKGROUND?",
 				test:function(t){
 					dohx.showInfo(
 								'<ul id="test60">'+
@@ -123,7 +123,7 @@
 			{
 				id:1200,
 				name:"Selectors: :nth-child(2n)",
-				expectedResult:"Do the first and third items now have a black background color?",
+				expectedResult:"Do the FIRST and THIRD items now have a BLACK BACKGROUND color?",
 				test:function(t){
 					dohx.showInfo(
 								'<ul id="test60">'+
@@ -154,8 +154,8 @@
 			},
 			{
 				id:2100,
-				name:"background-position: local (ONLY to be done if overflow: auto is supported)",
-				expectedResult:"Scroll the element. Does the image scroll with the element?",
+				name:"multiple background images",
+				expectedResult:"Do you see TWO IMAGES in the element?",
 				test:function(t){
 					dohx.showInfo('<div id="test2100">'+
 						'This is the element<br>This is the element<br>This is the element<br>'+
@@ -167,10 +167,10 @@
 			},
 			{
 				id:2200,
-				name:"multiple background images",
-				expectedResult:"Do you see two images in the element?",
+				name:"background-position: local (ONLY to be done if overflow: auto is supported)",
+				expectedResult:"Scroll the element. DOES THE IMAGE SCROLL with the element?",
 				test:function(t){
-					dohx.showInfo('<div id="test112">'+
+					dohx.showInfo('<div id="test2200">'+
 						'This is the element<br>This is the element<br>This is the element<br>'+
 						'This is the element<br>This is the element<br>This is the element<br>'+
 						'This is the element<br>This is the element<br>This is the element<br>'+
@@ -181,7 +181,7 @@
 			{
 				id:2300,
 				name:"border-radius",
-				expectedResult:"Does the element have rounded corners?",
+				expectedResult:"Does the element have ROUNDED CORNERS?",
 				test:function(t){
 					dohx.showInfo('<div id="test2300">'+
 						'This is the element<br>This is the element<br>This is the element<br>'+
@@ -192,7 +192,7 @@
 			{
 				id:2400,
 				name:"rgba colors",
-				expectedResult:"Is the text red?",
+				expectedResult:"Is the text RED?",
 				test:function(t){
 					dohx.showInfo('<div id="test114">'+
 						'This is the element<br>This is the element<br>This is the element<br>'+
@@ -203,7 +203,7 @@
 			{
 				id:2500,
 				name:"hsla colors",
-				expectedResult:"Is the text red?",
+				expectedResult:"Is the text RED?",
 				test:function(t){
 					dohx.showInfo('<div id="test115">'+
 						'This is the element<br>This is the element<br>This is the element<br>'+
@@ -214,7 +214,7 @@
 			{
 				id:2600,
 				name:"outline",
-				expectedResult:"Does the element have a red border?",
+				expectedResult:"Does the element have a RED BORDER?",
 				test:function(t){
 					dohx.showInfo('<div id="test2600">'+
 						'This is the element<br>This is the element<br>This is the element<br>'+
