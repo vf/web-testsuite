6454624f3e66473cd0e75f980f7e2043be586b08
wolfram.kriesing <wolfram.kriesing@1690c1e1-514a-e7be-8591-f3815e2cc7b6>
2011-01-05 16:56:16 +0000

    Added PPKs tests, partly.

---
+++ src/tests/w3c/css/animation.index.html.js
@@ -0,0 +1,5 @@
+
+// No features are needed for this test.
+var xml = xmlHelper.getXmlObject(config);
+
+xml.head.link += <link rel="stylesheet" href="tests/w3c/css/animation.css" type="text/css" />;
---
+++ src/tests/w3c/css/animation.js
@@ -0,0 +1,45 @@
+(function(){
+	
+	__toggleActive = function(obj){
+		if (/active/.test(obj.className)) {
+			obj.className = obj.className.replace(/active/,'');
+		}else{
+			obj.className += ' active';
+		}
+	}
+
+	
+	dohx.add({name:"CSS animations",
+		mqcExecutionOrderBaseOffset:510000, // This number is the base offset for the execution order, the test ID gets added. Never change this number unless you know what you are doing.
+		tests:[
+			{
+				id:100,
+				name:"Basics",
+				expectedResult:"Touch the test element. Does it change?",
+				test:function(t){
+					dohx.showInfo(
+						'<div class="animation" onclick="__toggleActive(this)" id="test100">This element should move</div>'
+					);
+				}
+			},
+//			{
+//				id:200,
+//				name:"animationstart event",
+//				instructions:["Touch the element."],
+//				test:function(t){
+//					dohx.showInfo(
+//						'<div class="transition" onclick="__toggleActive(this)" id="test200">This element should move</div>'
+//					);
+//window.onanimationstart =
+//  window.onanimationiteration =
+//  window.onanimationend =
+//  window.ontransitionend = function (e) {
+//	console.log('Pure ' + e.type);
+//}
+//				}
+//			},
+//*/
+		]
+	});
+
+})();
