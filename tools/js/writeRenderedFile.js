518ca8e85f0da2d2575eeb119de0a999f1d84fc1
wolfram.kriesing <wolfram.kriesing@1690c1e1-514a-e7be-8591-f3815e2cc7b6>
2011-01-05 16:55:59 +0000

    Comments

--- tools/js/writeRenderedFile.js
+++ tools/js/writeRenderedFile.js
@@ -2,9 +2,13 @@
 // This file writes the resulting XML into the appropriate "config.xml" file.
 //
 
+//XML.prettyPrinting = false;
 var s = ""+xml;
 // *** hacky "fixing" of the XML to be valid HTML
 if (/\.html$/.test(config.destinationFile)){
+	//
+	//	HTML post processing
+	//
 	// Make sure all <.../> tags become <...></...> tags, since that is what HTML understands.
 	s = s.replace(/<(\w+)([^<>]*)\/>/g, "<$1$2></$1>");
 }
