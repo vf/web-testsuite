ac719ee45f72799b9074b9061d114c4dbceb900f
wolfram.kriesing <wolfram.kriesing@1690c1e1-514a-e7be-8591-f3815e2cc7b6>
2011-02-09 16:15:00 +0000

    Added license

--- src/testList.js
+++ src/testList.js
@@ -1,3 +1,18 @@
+/*
+	Copyright 2010-2011 Vodafone Group Services GmbH
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+		http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
 var testList = {
 	
 	configXmlDependent:{
@@ -25,6 +40,10 @@ var testList = {
 			"tests/w3c/css/basics.js",
 			"tests/w3c/css/transform.js",
 			"tests/w3c/css/transition.js",
+			
+			"tests/w3c/mediaquery/orientation.js",
+			"tests/w3c/mediaquery/widthHeight.js",
+			"tests/w3c/mediaquery/misc.js",
 			
 			"tests/misc/uri/sms.js",
 			"tests/misc/uri/mailto.js",
