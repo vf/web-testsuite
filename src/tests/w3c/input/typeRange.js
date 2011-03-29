57002f9295d06defc299577a2c849a4d3012c04b
Wolfram Kriesing <wk@uxebu.com>
2011-03-29 17:14:32 +0200

    Added comments as "description" property, so we can also use it.

--- src/tests/w3c/input/typeRange.js
+++ src/tests/w3c/input/typeRange.js
@@ -4,13 +4,11 @@
 		mqcExecutionOrderBaseOffset:680000, // This number is the base offset for the execution order, the test ID gets added. Never change this number unless you know what you are doing.
 		tests:[
 			{
-				//
-				// Test if a UI component is available at all.
-				// Spec says:
-				// 		The input element represents a control for setting the element's value to a string representing a number
-				// 
 				id:100,
-				name:"Range slides/interface available?",
+				summary:"Range slides/interface available?",
+				description: "Test if a UI component is available at all.\
+					Spec says:\
+						The input element represents a control for setting the element's value to a string representing a number",
 				definedInSpecs:["http://www.w3.org/TR/html5/number-state.html#range-state"],
 				expectedResult:"Did you get a range control (i.e. a slider)?",
 				test:function(t){
@@ -18,12 +16,8 @@
 				}
 			},
 			{
-				//
-				// Spec says:
-				// 		??????????????
-				// 
 				id:200,
-				name:"Three values selectable?",
+				summary:"Three values selectable?",
 				definedInSpecs:["http://www.w3.org/TR/html5/number-state.html#range-state"],
 				instrutions:[
 					'Click "GO"!',
@@ -35,16 +29,14 @@
 				}
 			},
 			{
-				//
-				// Verify the auto-correction of the initial value.
-				// Spec says:
-				// 		When the element is suffering from a step mismatch, the user agent must round the element's
-				// 		value to the nearest number for which the element would not suffer from a step mismatch,
-				// 		and which is greater than or equal to the minimum, and, if the maximum is not less than
-				// 		the minimum, which is less than or equal to the maximum.
-				// 
 				id:300,
-				name:"Does the initial value get rounded properly (from 0.5 to 2)?",
+				summary:"Does the initial value get rounded properly (from 0.5 to 2)?",
+				description:"Verify the auto-correction of the initial value.\
+					Spec says:\
+						When the element is suffering from a step mismatch, the user agent must round the element's\
+						value to the nearest number for which the element would not suffer from a step mismatch,\
+						and which is greater than or equal to the minimum, and, if the maximum is not less than\
+						the minimum, which is less than or equal to the maximum.",
 				definedInSpecs:["http://www.w3.org/TR/html5/number-state.html#range-state"],
 				test:function(t){
 					dohx.showInfo('<input id="_input300" type="range" min=2 value=0.5 />');
@@ -54,17 +46,15 @@
 				}
 			},
 			{
-				//
-				// If the min=0, step=1 then a value of 1.5 should be rounded to 2.
-				// Verify the auto-correction of the initial value.
-				// Spec says:
-				// 		When the element is suffering from a step mismatch, the user agent must round the element's
-				// 		value to the nearest number for which the element would not suffer from a step mismatch,
-				// 		and which is greater than or equal to the minimum, and, if the maximum is not less than
-				// 		the minimum, which is less than or equal to the maximum.
-				// 
 				id:400,
-				name:"Does the initial value get rounded properly inside a range (from 1.5 to 2)?",
+				summary:"Does the initial value get rounded properly inside a range (from 1.5 to 2)?",
+				description:"If the min=0, step=1 then a value of 1.5 should be rounded to 2.\
+					Verify the auto-correction of the initial value.\
+					Spec says:\
+						When the element is suffering from a step mismatch, the user agent must round the element's\
+						value to the nearest number for which the element would not suffer from a step mismatch,\
+						and which is greater than or equal to the minimum, and, if the maximum is not less than\
+						the minimum, which is less than or equal to the maximum.",
 				definedInSpecs:["http://www.w3.org/TR/html5/number-state.html#range-state"],
 				test:function(t){
 					dohx.showInfo('<input id="_input400" type="range" step=1 min=0 value=1.5 />');
