914408f6fd5045350e4d8a0c4e66cad775eb1dde
Wolfram Kriesing <wk@uxebu.com>
2011-03-31 22:34:15 +0200

    embedjs update

--- src/js/embedjs-vodafone-apps-manager2.2.uncompressed.js
+++ src/js/embedjs-vodafone-apps-manager2.2.uncompressed.js
@@ -1910,52 +1910,67 @@ dojo.query = dojo._query = acme.query;
 
 
 /*********FILE**********
-/Users/cain/programming/uxebu/repositories/embedjs/src/query/chainable.js
+/Users/cain/programming/uxebu/repositories/embedjs/src/queryExtensions/ChainableNodeArray.js
 ********************/
 
 
 ;(function(){
-	
+	// Remember the old query function, so we can still call it.
 	var _oldQuery = embed.query;
-	
+	// Override embed.query() with a chainable version of itself.
 	embed.query = function(query, scope){
-		return new NodeList(_oldQuery.apply(embed, arguments));
-	}
+		return new embed.ChainableNodeArray(_oldQuery.apply(embed, arguments));
+	};
 	
-	// Extend the Array prototype for the NodeList to provide all methods that
+	// Extend the Array prototype for the ChainableNodeArray to provide all methods that
 	// are reachable by chainable functions.
-	var NodeList = function(arr){
-		var ret = []; // For some reason Array.apply(null, arguments) didn't work, so we push all from arr handish into ret, down there.
-		enhanceNodeList(ret);
-		if (arr){
-			for (var i=0, l=arr.length; i<l; i++){
-				ret.push(arr[i]);
-			}
-		}
+	embed.ChainableNodeArray = function(arr){
+		//var ret = Array.apply(null, arr);
+		var ret = [];
+		// "arr" is a NodeList object and WebKit is not able to use that as parameters to push(), ff can though.
+		// So let's explicitly convert the NodeList into an array.
+		ret.push.apply(ret, Array.prototype.slice.call(arr, 0));
+		makeChainable(ret);
 		return ret;
 	};
 	
-	function enhanceNodeList(obj){
-		var chainedFunctions = "attr addClass connect removeAttr removeClass style toggleClass place".split(" ");
+	function makeChainable(obj){
+		var chainedFunctions = ["attr", "addClass", "connect", "removeAttr", "removeClass", "style", "toggleClass", "place"];
 		for (var i=0, l=chainedFunctions.length, func; i<l; i++){
 			func = chainedFunctions[i];
 			// Create the functions on the object. I am sure this could be more efficiently done, i.e.
 			// on the prototype. Feel free to optimize it!
 			obj[func] = (function(func){
 				return function(){
-					var ret;
+					var argsAsArray = [].splice.call(arguments,0); // Convert arguments into an array, so we can use cancat() on it.
 					for (var i=0, l=this.length; i<l; i++){
 						// Concatenate this[i]+arguments into one array to be able to pass them as ONE array.
-						var argsAsArray = [].splice.call(arguments,0); // Convert arguments into an array, so we can use cancat() on it.
-						ret = embed[func].apply(embed, [this[i]].concat(argsAsArray));
+						// "this[i]" is the current node, since this is the array we are in, the array with all the nodes query() returned.
+						embed[func].apply(embed, [this[i]].concat(argsAsArray));
 					}
-					return ret; // Return the last return value.
+					return this; // Return the last return value.
+				}
+			})(func);
+		}
+		
+		// The array functions shall also always be enabled! Even the natively implemented once we have to convert their
+		// results back to a ChainableNodeArray.
+		var arrayFunctions = ["forEach", "map", "some", "every", "filter"];
+		for (var i=0, l=arrayFunctions.length, func; i<l; i++){
+			func = arrayFunctions[i];
+			obj[func] = (function(func){
+				return function(){
+					var argsAsArray = [].splice.call(arguments,0); // Convert arguments into an array, so we can use cancat() on it.
+					var ret = embed[func].apply(embed, [this].concat(argsAsArray));
+					// The result we get returned above is a native array, let's convert
+					// it into a chainable one again so the chaining can go on.
+					return new embed.ChainableNodeArray(ret);
 				}
 			})(func);
 		}
 	}
-	
 })();
+
 
 
 
