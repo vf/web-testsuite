b3d324f3496a70fc20b3c547bc4708a495daabff
Wolfram Kriesing <wk@uxebu.com>
2011-06-08 11:24:03 +0200

    Cleaning up a bit

--- src/tests/javascript/array/methods.js
+++ src/tests/javascript/array/methods.js
@@ -7,127 +7,127 @@
 			//
 			// concat
 			//
-			javascriptUtil.getExistsTest({id:100, object:[], property:"concat", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:100, object:[], property:"concat", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:101, object:[], property:"concat", expectedType:"function", dependsOn:[100], specs:[]}),
 			
 			//
 			// every
 			//
-			javascriptUtil.getExistsTest({id:200, object:[], property:"every", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:200, object:[], property:"every", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:210, object:[], property:"every", expectedType:"function", dependsOn:[200], specs:[]}),
 			
 			//
 			// filter
 			//
-			javascriptUtil.getExistsTest({id:300, object:[], property:"filter", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:300, object:[], property:"filter", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:310, object:[], property:"filter", expectedType:"function", dependsOn:[300], specs:[]}),
 			
 			//
 			// forEach
 			//
-			javascriptUtil.getExistsTest({id:400, object:[], property:"forEach", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:400, object:[], property:"forEach", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:410, object:[], property:"forEach", expectedType:"function", dependsOn:[400], specs:[]}),
 			
 			//
 			// indexOf
 			//
-			javascriptUtil.getExistsTest({id:500, object:[], property:"indexOf", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:500, object:[], property:"indexOf", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:510, object:[], property:"indexOf", expectedType:"function", dependsOn:[500], specs:[]}),
 			
 			//
 			// join
 			//
-			javascriptUtil.getExistsTest({id:600, object:[], property:"join", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:600, object:[], property:"join", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:610, object:[], property:"join", expectedType:"function", dependsOn:[600], specs:[]}),
 			
 			//
 			// lastIndexOf
 			//
-			javascriptUtil.getExistsTest({id:700, object:[], property:"lastIndexOf", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:700, object:[], property:"lastIndexOf", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:710, object:[], property:"lastIndexOf", expectedType:"function", dependsOn:[700], specs:[]}),
 			
 			//
 			// map
 			//
-			javascriptUtil.getExistsTest({id:800, object:[], property:"map", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:800, object:[], property:"map", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:810, object:[], property:"map", expectedType:"function", dependsOn:[800], specs:[]}),
 			
 			//
 			// pop
 			//
-			javascriptUtil.getExistsTest({id:900, object:[], property:"pop", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:900, object:[], property:"pop", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:910, object:[], property:"pop", expectedType:"function", dependsOn:[900], specs:[]}),
 			
 			//
 			// push
 			//
-			javascriptUtil.getExistsTest({id:1000, object:[], property:"push", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1000, object:[], property:"push", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1010, object:[], property:"push", expectedType:"function", dependsOn:[1000], specs:[]}),
 			
 			//
 			// reduce
 			//
-			javascriptUtil.getExistsTest({id:1100, object:[], property:"reduce", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1100, object:[], property:"reduce", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1110, object:[], property:"reduce", expectedType:"function", dependsOn:[1100], specs:[]}),
 			
 			//
 			// reduceRight
 			//
-			javascriptUtil.getExistsTest({id:1200, object:[], property:"reduceRight", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1200, object:[], property:"reduceRight", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1210, object:[], property:"reduceRight", expectedType:"function", dependsOn:[1200], specs:[]}),
 			
 			//
 			// reverse
 			//
-			javascriptUtil.getExistsTest({id:1300, object:[], property:"reverse", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1300, object:[], property:"reverse", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1310, object:[], property:"reverse", expectedType:"function", dependsOn:[1300], specs:[]}),
 			
 			//
 			// slice
 			//
-			javascriptUtil.getExistsTest({id:1400, object:[], property:"slice", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1400, object:[], property:"slice", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1410, object:[], property:"slice", expectedType:"function", dependsOn:[1400], specs:[]}),
 			
 			//
 			// shift
 			//
-			javascriptUtil.getExistsTest({id:1500, object:[], property:"shift", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1500, object:[], property:"shift", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1510, object:[], property:"shift", expectedType:"function", dependsOn:[1500], specs:[]}),
 			
 			//
 			// some
 			//
-			javascriptUtil.getExistsTest({id:1600, object:[], property:"some", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1600, object:[], property:"some", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1610, object:[], property:"some", expectedType:"function", dependsOn:[1600], specs:[]}),
 			
 			//
 			// sort
 			//
-			javascriptUtil.getExistsTest({id:1700, object:[], property:"sort", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1700, object:[], property:"sort", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1710, object:[], property:"sort", expectedType:"function", dependsOn:[1700], specs:[]}),
 			
 			//
 			// splice
 			//
-			javascriptUtil.getExistsTest({id:1800, object:[], property:"splice", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1800, object:[], property:"splice", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1810, object:[], property:"splice", expectedType:"function", dependsOn:[1800], specs:[]}),
 			
 			//
 			// toSource
 			//
-			javascriptUtil.getExistsTest({id:1900, object:[], property:"toSource", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1900, object:[], property:"toSource", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1910, object:[], property:"toSource", expectedType:"function", dependsOn:[1900], specs:[]}),
 			
 			//
 			// toString
 			//
-			javascriptUtil.getExistsTest({id:2000, object:[], property:"toString", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:2000, object:[], property:"toString", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:2100, object:[], property:"toString", expectedType:"function", dependsOn:[2000], specs:[]}),
 			
 			//
 			// unshift
 			//
-			javascriptUtil.getExistsTest({id:2100, object:[], property:"unshift", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:2100, object:[], property:"unshift", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:2110, object:[], property:"unshift", expectedType:"function", dependsOn:[2100], specs:[]}),
 //*/
 		]
--- src/tests/javascript/string/methods.js
+++ src/tests/javascript/string/methods.js
@@ -7,26 +7,26 @@
 			//
 			// charAt
 			//
-			javascriptUtil.getExistsTest({id:100, object:"", property:"charAt", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:100, object:"", property:"charAt", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:101, object:"", property:"charAt", expectedType:"function", dependsOn:[100], specs:[]}),
 			
 			//
 			// charCodeAt
 			//
-			javascriptUtil.getExistsTest({id:200, object:"", property:"charCodeAt", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:200, object:"", property:"charCodeAt", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:210, object:"", property:"charCodeAt", expectedType:"function", dependsOn:[200], specs:[]}),
 			
 			//
 			// concat
 			//
-			javascriptUtil.getExistsTest({id:300, object:"", property:"concat", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:300, object:"", property:"concat", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:310, object:"", property:"concat", expectedType:"function", dependsOn:[300], specs:[]}),
 			
 			//
 			// indexOf
 			// (there used to be a time when indexOf() didnt work in IE, so this might be irrelevant on most modern platforms)
 			//
-			javascriptUtil.getExistsTest({id:400, object:"", property:"indexOf", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:400, object:"", property:"indexOf", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:410, object:"", property:"indexOf", expectedType:"function", dependsOn:[400], specs:[]}),
 			{
 				id:420,
@@ -76,31 +76,31 @@
 			//
 			// lastIndexOf
 			//
-			javascriptUtil.getExistsTest({id:500, object:"", property:"lastIndexOf", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:500, object:"", property:"lastIndexOf", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:510, object:"", property:"lastIndexOf", expectedType:"function", dependsOn:[500], specs:[]}),
 			
 			//
 			// localeCompare
 			//
-			javascriptUtil.getExistsTest({id:600, object:"", property:"localeCompare", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:600, object:"", property:"localeCompare", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:610, object:"", property:"localeCompare", expectedType:"function", dependsOn:[600], specs:[]}),
 			
 			//
 			// match
 			//
-			javascriptUtil.getExistsTest({id:700, object:"", property:"match", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:700, object:"", property:"match", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:710, object:"", property:"match", expectedType:"function", dependsOn:[700], specs:[]}),
 			
 			//
 			// quote
 			//
-			javascriptUtil.getExistsTest({id:800, object:"", property:"quote", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:800, object:"", property:"quote", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:810, object:"", property:"quote", expectedType:"function", dependsOn:[800], specs:[]}),
 			
 			//
 			// replace
 			//
-			javascriptUtil.getExistsTest({id:900, object:"", property:"replace", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:900, object:"", property:"replace", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:910, object:"", property:"replace", expectedType:"function", dependsOn:[900], specs:[]}),
 			{
 				id:920,
@@ -117,91 +117,91 @@
 			//
 			// search
 			//
-			javascriptUtil.getExistsTest({id:1000, object:"", property:"search", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1000, object:"", property:"search", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1010, object:"", property:"search", expectedType:"function", dependsOn:[1000], specs:[]}),
 			
 			//
 			// slice
 			//
-			javascriptUtil.getExistsTest({id:1100, object:"", property:"slice", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1100, object:"", property:"slice", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1110, object:"", property:"slice", expectedType:"function", dependsOn:[1100], specs:[]}),
 			
 			//
 			// split
 			//
-			javascriptUtil.getExistsTest({id:1200, object:"", property:"split", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1200, object:"", property:"split", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1210, object:"", property:"split", expectedType:"function", dependsOn:[1200], specs:[]}),
 			
 			//
 			// substr
 			//
-			javascriptUtil.getExistsTest({id:1300, object:"", property:"substr", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1300, object:"", property:"substr", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1310, object:"", property:"substr", expectedType:"function", dependsOn:[1300], specs:[]}),
 			
 			//
 			// substring
 			//
-			javascriptUtil.getExistsTest({id:1400, object:"", property:"substring", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1400, object:"", property:"substring", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1410, object:"", property:"substring", expectedType:"function", dependsOn:[1400], specs:[]}),
 			
 			//
 			// toLocaleLowerCase
 			//
-			javascriptUtil.getExistsTest({id:1500, object:"", property:"toLocaleLowerCase", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1500, object:"", property:"toLocaleLowerCase", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1510, object:"", property:"toLocaleLowerCase", expectedType:"function", dependsOn:[1500], specs:[]}),
 			
 			//
 			// toLocaleUpperCase
 			//
-			javascriptUtil.getExistsTest({id:1600, object:"", property:"toLocaleUpperCase", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1600, object:"", property:"toLocaleUpperCase", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1610, object:"", property:"toLocaleUpperCase", expectedType:"function", dependsOn:[1600], specs:[]}),
 			
 			//
 			// toLowerCase
 			//
-			javascriptUtil.getExistsTest({id:1700, object:"", property:"toLowerCase", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1700, object:"", property:"toLowerCase", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1710, object:"", property:"toLowerCase", expectedType:"function", dependsOn:[1700], specs:[]}),
 			
 			//
 			// toSource
 			//
-			javascriptUtil.getExistsTest({id:1800, object:"", property:"toSource", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1800, object:"", property:"toSource", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1810, object:"", property:"toSource", expectedType:"function", dependsOn:[1800], specs:[]}),
 			
 			//
 			// toString
 			//
-			javascriptUtil.getExistsTest({id:1900, object:"", property:"toString", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:1900, object:"", property:"toString", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:1910, object:"", property:"toString", expectedType:"function", dependsOn:[1900], specs:[]}),
 			
 			//
 			// toUpperCase
 			//
-			javascriptUtil.getExistsTest({id:2000, object:"", property:"toUpperCase", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:2000, object:"", property:"toUpperCase", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:2100, object:"", property:"toUpperCase", expectedType:"function", dependsOn:[2000], specs:[]}),
 			
 			//
 			// trim
 			//
-			javascriptUtil.getExistsTest({id:2100, object:"", property:"trim", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:2100, object:"", property:"trim", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:2110, object:"", property:"trim", expectedType:"function", dependsOn:[2100], specs:[]}),
 			
 			//
 			// trimLeft
 			//
-			javascriptUtil.getExistsTest({id:2200, object:"", property:"trimLeft", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:2200, object:"", property:"trimLeft", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:2210, object:"", property:"trimLeft", expectedType:"function", dependsOn:[2200], specs:[]}),
 			
 			//
 			// trimRight
 			//
-			javascriptUtil.getExistsTest({id:2300, object:"", property:"trimRight", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:2300, object:"", property:"trimRight", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:2310, object:"", property:"trimRight", expectedType:"function", dependsOn:[2300], specs:[]}),
 			
 			//
 			// valueOf
 			//
-			javascriptUtil.getExistsTest({id:2400, object:"", property:"valueOf", expectedType:"", specs:[]}),
+			javascriptUtil.getExistsTest({id:2400, object:"", property:"valueOf", specs:[]}),
 			javascriptUtil.getTypeCheckTest({id:2410, object:"", property:"valueOf", expectedType:"function", dependsOn:[2400], specs:[]}),
 //*/
 		]
