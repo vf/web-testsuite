eb651b064019073bbefd1c27f356a94a13683e2d
Wolfram Kriesing <wk@uxebu.com>
2011-06-17 16:01:44 +0200

    Input type camera

---
+++ src/tests/w3c/input/typeCamera.js
@@ -0,0 +1,22 @@
+(function(){
+
+	dohx.add({name:"Number inputs (type=file, capture=camera)",
+		mqcExecutionOrderBaseOffset:900000, // This number is the base offset for the execution order, the test ID gets added. Never change this number unless you know what you are doing.
+		tests:[
+			{
+				id:100,
+				name:"Camera input?.",
+				definedInSpecs:[""],
+				instructions:[
+					"Click 'GO'!",
+					"Click the input element."
+				],
+				expectedResult:"Did a camera input open?",
+				test:function(t){
+					dohx.showInfo('<form><input type="file" accept="image/*;capture=camera"></input></form>');
+				}
+			},
+//*/
+		]
+	});
+})();
