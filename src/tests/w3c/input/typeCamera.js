7328e43a38608c626fd556335db4a4375e38160e
Wolfram Kriesing <wk@uxebu.com>
2011-06-20 23:50:24 +0200

    Added DAP way

--- src/tests/w3c/input/typeCamera.js
+++ src/tests/w3c/input/typeCamera.js
@@ -5,15 +5,28 @@
 		tests:[
 			{
 				id:100,
-				name:"Camera input?.",
-				definedInSpecs:[""],
+				name:'Camera input (using <input type="file" accept="image/*;capture=camera">)?',
+				definedInSpecs:[], // Seems not to be defined
 				instructions:[
 					"Click 'GO'!",
 					"Click the input element."
 				],
 				expectedResult:"Did a camera input open?",
 				test:function(t){
-					dohx.showInfo('<form><input type="file" accept="image/*" capture="camera" id="capture"></form>');
+					dohx.showInfo('<form><input type="file" accept="image/*;capture=camera"></form>');
+				}
+			},
+			{
+				id:200,
+				name:'Camera input, the DAP way (<input type="file" accept="image/*" capture="camera")?',
+				definedInSpecs:["http://www.w3.org/TR/html-media-capture/#attributes"],
+				instructions:[
+					"Click 'GO'!",
+					"Click the input element."
+				],
+				expectedResult:"Did a camera input open?",
+				test:function(t){
+					dohx.showInfo('<form><input type="file" accept="image/*" capture="camera"></form>');
 				}
 			},
 //*/
